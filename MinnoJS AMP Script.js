a4c1aad31f171044d67e89de0c5c7bca9830cc5f
yahel570 <76042106+yahel570@users.noreply.github.com>
2020-12-17 10:47:36 +0200

    Update MinnoJS AMP Script.js

--- MinnoJS AMP Script.js
+++ MinnoJS AMP Script.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://yahel570.github.io/AMP---Primes/MinnoJS%20AMP%20Script.js'], function(APIConstructor, ampExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
 
 	var API = new APIConstructor();
 	
