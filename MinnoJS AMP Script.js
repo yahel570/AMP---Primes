5dd4c6f10862d97b674fb114ee992040fa0d221d
yahel570 <76042106+yahel570@users.noreply.github.com>
2020-12-17 09:45:41 +0200

    Update and rename MinnoJS AMP Script to MinnoJS AMP Script.js

--- MinnoJS AMP Script.js
+++ MinnoJS AMP Script.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
+define(['pipAPI', 'https://github.com/yahel570/AMP---Primes/blob/main/MinnoJS%20AMP%20Script.js'], function(APIConstructor, ampExtension){
 
 	var API = new APIConstructor();
 	
