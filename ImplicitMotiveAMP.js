adc1422933b4bd47351214c7a62c7900cd09e453
yahel570 <76042106+yahel570@users.noreply.github.com>
2020-12-23 13:48:28 +0200

    Update ImplicitMotiveAMP.js

--- ImplicitMotiveAMP.js
+++ ImplicitMotiveAMP.js
@@ -37,7 +37,8 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 					nameForLogging : 'Affiliation Approach', //Will be used in the logging
 					//An array of all media objects for this category.
           mediaArray : [{image : 'AH01.jpg'}, {image : 'AH02.jpg'}, {image : 'AH03.jpg'}, {image : 'AH04.jpg'}, {image : 'AH05.jpg'}, {image : 'AH06.jpg'}, {image : 'AH07.jpg'}, {image : 'AH08.jpg'}]
-				}, 				{
+				}, 
+				{
 					nameForFeedback : 'Power Approach',  //Will be used in the user feedback 
 					nameForLogging : 'Power Approach', //Will be used in the logging
 					//An array of all media objects for this category.
@@ -53,7 +54,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 					nameForFeedback : 'Affiliation Avoidance',  //Will be used in the user feedback 
 					nameForLogging : 'Affiliation Avoidance', //Will be used in the logging
           mediaArray : [{image : 'AF01.jpg'}, {image : 'AF02.jpg'}, {image : 'AF03.jpg'}, {image : 'AF04.jpg'}, {image : 'AF05.jpg'}, {image : 'AF06.jpg'}, {image : 'AF07.jpg'}, {image : 'AF08.jpg'}]				
-         }
+				}
          ],
 			examplePrimeStimulus : 
 			{
